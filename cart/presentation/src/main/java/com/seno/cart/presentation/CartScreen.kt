e028a5d36ad466f7d2ea07b24b2efda01920fa21
extractive-mana-pulse <invoker1441@gmail.com>
2025-10-26 13:46:00 +0500

    UI is implemented, logic left.

--- cart/presentation/src/main/java/com/seno/cart/presentation/CartScreen.kt
+++ cart/presentation/src/main/java/com/seno/cart/presentation/CartScreen.kt
@@ -1,22 +1,30 @@
 package com.seno.cart.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.seno.cart.presentation.components.CartToppingCard
 import com.seno.cart.presentation.components.EmptyCartComponent
 import com.seno.core.presentation.components.LoadingComponent
 import com.seno.core.presentation.components.card.ProductCard
 import com.seno.core.presentation.theme.LazyPizzaTheme
-import com.seno.core.presentation.theme.body_1_medium
-import com.seno.core.presentation.theme.textPrimary
+import com.seno.core.presentation.theme.label_2_semiBold
+import com.seno.core.presentation.theme.textSecondary
 
 @Composable
 fun CartScreen(
@@ -25,59 +33,76 @@ fun CartScreen(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            modifier = Modifier
-                .padding(vertical = 21.dp),
-            text = "Cart",
-            style = body_1_medium.copy(
-                color = textPrimary
+            .fillMaxSize()
+            .padding(
+                start = 16.dp,
+                end = 16.dp,
             ),
-        )
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(
-                    start = 16.dp,
-                    end = 16.dp,
-                    bottom = 20.dp,
-                ),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            if (state.isLoading) {
-                LoadingComponent(text = "Getting your cart, please wait ...")
-            } else if (state.cartItems.isEmpty()) {
-                EmptyCartComponent(
-                    modifier = Modifier
-                        .padding(top = 120.dp),
-                    onBackToMenuClick = { onAction(CartActions.OnNavigateToMenuClick) }
-                )
-            } else {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    items(state.cartItems) { cartItem ->
-                        ProductCard(
-                            imageUrl = cartItem.image,
-                            productName = cartItem.name,
-                            productPrice = cartItem.price,
-                            quantity = cartItem.quantity,
-                            onQuantityChange = { quantity ->
-                                onAction(
-                                    CartActions.OnCartItemQuantityChange(
-                                        reference = cartItem.reference,
-                                        quantity = quantity,
-                                    )
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        if (state.isLoading) {
+            LoadingComponent(text = "Getting your cart, please wait ...")
+        } else if (state.cartItems.isEmpty()) {
+            EmptyCartComponent(
+                modifier = Modifier
+                    .padding(top = 120.dp),
+                onBackToMenuClick = { onAction(CartActions.OnNavigateToMenuClick) }
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            ) {
+                items(state.cartItems) { cartItem ->
+                    ProductCard(
+                        imageUrl = cartItem.image,
+                        productName = cartItem.name,
+                        productPrice = cartItem.price,
+                        quantity = cartItem.quantity,
+                        onQuantityChange = { quantity ->
+                            onAction(
+                                CartActions.OnCartItemQuantityChange(
+                                    reference = cartItem.reference,
+                                    quantity = quantity,
                                 )
-                            },
-                            onDeleteClicked = { onAction(CartActions.OnDeleteCartItemClick(cartItem.reference)) },
-                            extraToppings = cartItem.extraToppingsRelated,
+                            )
+                        },
+                        onDeleteClicked = { onAction(CartActions.OnDeleteCartItemClick(cartItem.reference)) },
+                        extraToppings = cartItem.extraToppingsRelated,
+                    )
+                }
+                item {
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    // Recommended section
+                    if (state.cartItems.isNotEmpty()) {
+                        Text(
+                            text = stringResource(R.string.recommended_title).uppercase(),
+                            style = label_2_semiBold.copy(
+                                color = textSecondary
+                            ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 8.dp)
                         )
+
+                        LazyRow(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            items(state.cartItems) { cartItem ->
+                                CartToppingCard(
+                                    imageUrl = cartItem.image,
+                                    cartItem = cartItem,
+                                    extraToppings = cartItem.extraToppingsRelated,
+                                    onClick = {}
+                                )
+                            }
+                        }
                     }
+                    Spacer(modifier = Modifier.height(24.dp))
+
                 }
             }
         }
---
+++ cart/presentation/src/main/java/com/seno/cart/presentation/components/CartToppingtCard.kt
@@ -0,0 +1,143 @@
+package com.seno.cart.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.seno.core.domain.product.ProductType
+import com.seno.core.presentation.R
+import com.seno.core.presentation.model.CartItemUI
+import com.seno.core.presentation.theme.body_1_regular
+import com.seno.core.presentation.theme.outline50
+import com.seno.core.presentation.theme.primary
+import com.seno.core.presentation.theme.textPrimary
+import com.seno.core.presentation.theme.textSecondary
+import com.seno.core.presentation.theme.title_1_semiBold
+import com.seno.core.presentation.utils.formatToPrice
+
+@Composable
+internal fun CartToppingCard(
+    modifier: Modifier = Modifier,
+    imageUrl: String,
+    onClick: () -> Unit,
+    cartItem: CartItemUI,
+    extraToppings: List<String>,
+) {
+    Column(
+        modifier = modifier
+            .width(160.dp)
+            .background(
+                color = MaterialTheme.colorScheme.surfaceContainerHigh,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(2.dp),
+    ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .padding(start = 1.dp, top = 1.dp, bottom = 8.dp, end = 1.dp)
+                .background(
+                    color = MaterialTheme.colorScheme.surfaceContainerHighest,
+                    shape = RoundedCornerShape(
+                        topStart = 12.dp,
+                        bottomStart = 0.dp,
+                        topEnd = 12.dp,
+                        bottomEnd = 0.dp
+                    )
+                ),
+            contentScale = ContentScale.Fit
+        )
+        if (extraToppings.isNotEmpty()) {
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = cartItem.extraToppingsRelated.joinToString(","),
+                style = body_1_regular.copy(
+                    color = textSecondary
+                ),
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "$${cartItem.price.formatToPrice()}",
+                style = title_1_semiBold.copy(
+                    color = textPrimary
+                )
+            )
+            IconButton(
+                onClick = {
+                    // handle the logic when item is selected add to the list of items in the cart,
+                    // once it's selected remove from recommended list.
+                    onClick()
+                },
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .border(
+                        width = 1.dp,
+                        color = outline50,
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(4.dp)
+                    .size(22.dp)
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.plus_ic),
+                    contentDescription = "Increase",
+                    tint = primary
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CartToppingCardPreview() {
+    CartToppingCard(
+        modifier = Modifier,
+        imageUrl = "",
+        onClick = {},
+        cartItem = CartItemUI(
+            reference = "",
+            image = "",
+            name = "",
+            price = 3.12,
+            type = ProductType.EXTRA_TOPPING,
+        ),
+        extraToppings = listOf(
+            "BBQ Sauce",
+        )
+    )
+}
