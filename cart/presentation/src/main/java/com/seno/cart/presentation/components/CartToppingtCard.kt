44129ceff6fc2bfa709f493b5daf74862b9a4611
extractive-mana-pulse <invoker1441@gmail.com>
2025-10-27 19:17:28 +0500

    fixed, paddings

--- cart/presentation/src/main/java/com/seno/cart/presentation/components/CartToppingtCard.kt
+++ cart/presentation/src/main/java/com/seno/cart/presentation/components/CartToppingtCard.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
