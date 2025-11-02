49d4756da8d4cdc04de5265013c6ca4afcbb4843
galahseno <galahsenoadjie@gmail.com>
2025-11-02 15:13:52 +0700

    [M2-011] workflows

--- core/presentation/src/main/java/com/seno/core/presentation/utils/DeviceConfiguration.kt
+++ core/presentation/src/main/java/com/seno/core/presentation/utils/DeviceConfiguration.kt
@@ -1,8 +1,6 @@
 package com.seno.core.presentation.utils
 
-import androidx.window.core.layout.WindowHeightSizeClass
 import androidx.window.core.layout.WindowSizeClass
-import androidx.window.core.layout.WindowWidthSizeClass
 
 enum class DeviceConfiguration {
     MOBILE_PORTRAIT,
@@ -14,15 +12,32 @@ enum class DeviceConfiguration {
     fun isTablet() = this == TABLET_PORTRAIT || this == TABLET_LANDSCAPE
 
     companion object {
+        private const val WIDTH_COMPACT_THRESHOLD = 600
+        private const val WIDTH_MEDIUM_THRESHOLD = 840
+
+        private const val HEIGHT_COMPACT_THRESHOLD = 480
+        private const val HEIGHT_MEDIUM_THRESHOLD = 900
+
         fun fromWindowSizeClass(windowSizeClass: WindowSizeClass): DeviceConfiguration {
-            val widthSizeClass = windowSizeClass.windowWidthSizeClass
-            val heightSizeClass = windowSizeClass.windowHeightSizeClass
+            val isCompactWidth = !windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_COMPACT_THRESHOLD)
+            val isMediumWidth =
+                windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_COMPACT_THRESHOLD) &&
+                    !windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_MEDIUM_THRESHOLD)
+            val isExpandedWidth = windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_MEDIUM_THRESHOLD)
+
+            val isCompactHeight =
+                !windowSizeClass.isHeightAtLeastBreakpoint(HEIGHT_COMPACT_THRESHOLD)
+            val isMediumHeight =
+                windowSizeClass.isHeightAtLeastBreakpoint(HEIGHT_COMPACT_THRESHOLD) &&
+                    !windowSizeClass.isHeightAtLeastBreakpoint(HEIGHT_MEDIUM_THRESHOLD)
+            val isExpandedHeight =
+                windowSizeClass.isHeightAtLeastBreakpoint(HEIGHT_MEDIUM_THRESHOLD)
+
             return when {
-                widthSizeClass == WindowWidthSizeClass.COMPACT && heightSizeClass == WindowHeightSizeClass.MEDIUM -> MOBILE_PORTRAIT
-                widthSizeClass == WindowWidthSizeClass.COMPACT && heightSizeClass == WindowHeightSizeClass.EXPANDED -> MOBILE_PORTRAIT
-                widthSizeClass == WindowWidthSizeClass.EXPANDED && heightSizeClass == WindowHeightSizeClass.COMPACT -> MOBILE_LANDSCAPE
-                widthSizeClass == WindowWidthSizeClass.MEDIUM && heightSizeClass == WindowHeightSizeClass.EXPANDED -> TABLET_PORTRAIT
-                widthSizeClass == WindowWidthSizeClass.EXPANDED && heightSizeClass == WindowHeightSizeClass.MEDIUM -> TABLET_LANDSCAPE
+                isCompactWidth && (isMediumHeight || isExpandedHeight) -> MOBILE_PORTRAIT
+                isExpandedWidth && isCompactHeight -> MOBILE_LANDSCAPE
+                isMediumWidth && isExpandedHeight -> TABLET_PORTRAIT
+                isExpandedWidth && isMediumHeight -> TABLET_LANDSCAPE
                 else -> DESKTOP
             }
         }
